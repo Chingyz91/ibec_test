293954448b802e2cf5d4ad160e4bf5104d93891b
Chingyz <77848176+Chingyz91@users.noreply.github.com>
2021-01-25 12:55:30 +0600

    some changes

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,16 +1,18 @@
 <template>
   <section>
     <h1>Наши пользователи</h1>
-        <a href="#"><li>Leanne Graham</li></a>
-        <a href="#"><li>Ervin Howell</li></a>
-        <a href="#"><li>Clementine Bauch</li></a>
-        <a href="#"><li>Patricia Lebsack</li></a>
-        <a href="#"><li>Chelsey Dietrich</li></a>
-        <a href="#"><li>Mrs. Dennis Schulist</li></a>
-        <a href="#"><li>Kurtis Weissnat</li></a>
-        <a href="#"><li>Nicholas Runolfsdottir V</li></a>
-        <a href="#"><li>Glenna Reichert</li></a>
-        <a href="#"><li>Clementina DuBuque</li></a>
+    <ul>
+        <li>Leanne Graham</li>
+        <li>Ervin Howell</li>
+        <li>Clementine Bauch</li>
+        <li>Patricia Lebsack</li>
+        <li>Chelsey Dietrich</li>
+        <li>Mrs. Dennis Schulist</li>
+        <li>Kurtis Weissnat</li>
+        <li>Nicholas Runolfsdottir V</li>
+        <li>Glenna Reichert</li>
+        <li>Clementina DuBuque</li>
+    </ul>
   </section>
 </template>
 
